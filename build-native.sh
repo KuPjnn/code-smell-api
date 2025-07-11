c33a1c4a197b9158ae50c2522b1febe0846c5b53
Code Smell <code.smell@gmail.com>
2025-07-12 01:49:56 +0700

    optimize resource to build native

--- build-native.sh
+++ build-native.sh
@@ -9,7 +9,8 @@ docker run --rm -it \
   -v $PWD:/workspace:rw \
   -w /workspace \
   vegardit/graalvm-maven:latest-java21 \
-  mvn clean install -Pnative \
+  mvn clean package -Pnative -DskipTests -DskipITs \
+  -Dquarkus.native.skip-jar=true \
   -Dquarkus.native.native-image-xmx=4g \
   -Dquarkus.native.additional-build-args=--parallelism=4
 
